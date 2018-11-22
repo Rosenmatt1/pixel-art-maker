183434ef28195213c7e5518ed19322aeb670473b
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2018-11-21 17:52:59 -0700

    Added color pallete and mario image

--- app.js
+++ app.js
@@ -11,92 +11,97 @@ var box9 = document.getElementById("box-9")
 var pickBlue = document.getElementById("blue")
 var pickRed = document.getElementById("red")
 var pickYellow = document.getElementById("yellow")
+var pickWhite = document.getElementById("white")
 let color = ""
+let currentColor = document.getElementById("currentColor")
 
 
 box1.addEventListener("click", function() {
 
   box1.classList.toggle(color)
-  box1.classList.toggle("box")
+  // event.target.classList = color
 
 })
 
 box2.addEventListener("click", function() {
 
   box2.classList.toggle(color)
-  box2.classList.toggle("box")
+  // event.target.classList = color
   
 })
 
 box3.addEventListener("click", function() {
 
   box3.classList.toggle(color)
-  box3.classList.toggle("box")
+  // event.target.classList = color
   
 })
 
 box4.addEventListener("click", function() {
 
   box4.classList.toggle(color)
-  box4.classList.toggle("box")
+  // event.target.classList = color
   
 })
 
 box5.addEventListener("click", function() {
 
   box5.classList.toggle(color)
-  box5.classList.toggle("box")
+  // event.target.classList = color
   
 })
 
 box6.addEventListener("click", function() {
 
   box6.classList.toggle(color)
-  box6.classList.toggle("box")
+  // event.target.classList = color
   
 })
 
 box7.addEventListener("click", function() {
 
   box7.classList.toggle(color)
-  box7.classList.toggle("box")
- 
+  // event.target.classList = color
 })
 
 box8.addEventListener("click", function() {
 
   box8.classList.toggle(color)
-  box8.classList.toggle("box")
+  // event.target.classList = color
   
 })
 
-box9.addEventListener("click", function() {
+box9.addEventListener("click", function(event) {
 
   box9.classList.toggle(color)
-  box9.classList.toggle("box")
- 
+  // event.target.classList = color
+  
+  
 })
 
   pickBlue.addEventListener("click", function(event) {
     color = "blue"
+    currentColor.classList = color
     
   })
 
   pickRed.addEventListener("click", function(event) {
     color = "red"
+    currentColor.classList = color
    
   })
 
   pickYellow.addEventListener("click", function(event) {
     color = "yellow"
-   
+    currentColor.classList = color
+  })
+
+  pickWhite.addEventListener("click", function(event) {
+    color = "white"
+    currentColor.classList = color
   })
 
 
-  // //   pickBlue.event.target("blue")
-
-    // pickBlue.classList.toggle(color)
-    // picklue.classList.toggle("box")
 
 })
 
